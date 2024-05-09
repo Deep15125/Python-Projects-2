78c597688164b4f6520a586cd8d49b613fa12af6
Deep15125 <deepgupta15125@gmail.com>
2024-05-09 12:20:53 +0530

    This is Basic File Handling where we modify the text in txt file

---
+++ Task3.py
@@ -0,0 +1,13 @@
+def read_and_replace(file_path, old_word, new_word):
+    with open(file_path, 'r+') as file:
+        data = file.read()
+        data = data.replace(old_word, new_word)
+        file.seek(0)
+        file.write(data)
+        file.truncate()
+
+file_path = 'example.txt'
+old_word = str(input("Enter the Word you want to change:"))
+new_word = str(input("Enter the new word to change:"))
+
+read_and_replace(file_path, old_word, new_word)
